d27d1724d45849ec7be889305110ac0256640e47
Angelina Ning <ning.angelina@gmail.com>
2024-09-14 23:13:56 -0400

    updated home page

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../stylesheets/HomePage.css';
+import image from '../../dist/assets/rotating_globe.png';
 
 const HomePage = () => {
   return (
@@ -9,7 +10,7 @@ const HomePage = () => {
       <p className="subheader">Unbiasing today's media</p>
       <Link to="/chat" className="button">ACCESS NOW</Link>
       <div className="image-container">
-        <img src="/path/to/your/image.jpg" alt="Large descriptive image" className="image" />
+        <img src={image} alt="Main home page img" className="image" />
       </div>
     </div>
   );

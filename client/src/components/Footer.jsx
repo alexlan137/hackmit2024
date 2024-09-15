a6a785cd3a21b761a8101ffe779b8597947ad4c8
Angelina Ning <ning.angelina@gmail.com>
2024-09-15 01:18:58 -0400

    merging commits

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,13 +4,8 @@ import "../stylesheets/Footer.css"; // We'll create this next
 const Footer = () => {
   return (
     <div className="footer">
-<<<<<<< HEAD
         <h2 className="company-name">clarity.ai</h2>
         <p className="copyright">© 2024</p>
-=======
-      <h2 className="company-name">SSOT</h2>
-      <p className="copyright">© 2024</p>
->>>>>>> 425597646fd9b2d9a93c64a801481b49c230769e
     </div>
   );
 };
